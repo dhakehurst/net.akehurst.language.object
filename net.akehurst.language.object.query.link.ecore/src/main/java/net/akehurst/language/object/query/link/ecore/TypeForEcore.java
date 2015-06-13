4f418ea42ca0825b006e5403f73e699f019afd4e
Dr. David H. Akehurst <david.akehurst@itemis.de>
2015-06-13 18:25:09 +0200

    updates

--- net.akehurst.language.object.query.link.ecore/src/main/java/net/akehurst/language/object/query/link/ecore/TypeForEcore.java
+++ net.akehurst.language.object.query.link.ecore/src/main/java/net/akehurst/language/object/query/link/ecore/TypeForEcore.java
@@ -1,25 +1,18 @@
 /*
  * Copyright (c) 2015 D. David H. Akehurst
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the
+ * License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
 package net.akehurst.language.object.query.link.ecore;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import org.eclipse.emf.ecore.EClassifier;
-import org.eclipse.emf.ecore.ETypeParameter;
 
 import net.akehurst.language.object.query.link.TypeContext;
 import net.akehurst.language.object.type.Operation;
@@ -71,7 +64,11 @@ public class TypeForEcore implements Type {
 	}
 
 	public Property getProperty(String name, Type... qualifierTypes) {
-		// TODO Auto-generated method stub
+		if (this.eClassifier instanceof EClass) {
+			EClass cls = (EClass) this.eClassifier;
+			EStructuralFeature fe = cls.getEStructuralFeature(name);
+			return new PropertyForEcore(this.context, fe);
+		}
 		return null;
 	}
 
--- net.akehurst.language.object.query.link.ecore/src/main/java/net/akehurst/language/object/query/link/ecore/ValueContextForEcore.java
+++ net.akehurst.language.object.query.link.ecore/src/main/java/net/akehurst/language/object/query/link/ecore/ValueContextForEcore.java
@@ -1,17 +1,13 @@
 /*
  * Copyright (c) 2015 D. David H. Akehurst
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the
+ * License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+ * CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
  */
 package net.akehurst.language.object.query.link.ecore;
 
@@ -20,13 +16,11 @@ import net.akehurst.language.object.query.link.ValueContext;
 public class ValueContextForEcore implements ValueContext {
 
 	public Object getStringLiteral(String symbol) {
-		// TODO Auto-generated method stub
-		return null;
+		return symbol;
 	}
 
 	public Object getBooleanLiteral(String symbol) {
-		// TODO Auto-generated method stub
-		return null;
+		return Boolean.parseBoolean(symbol);
 	}
 
 	public Object getNumberLiteral(String symbol) {
@@ -35,8 +29,7 @@ public class ValueContextForEcore implements ValueContext {
 	}
 
 	public Object getObject(Object object) {
-		// TODO Auto-generated method stub
-		return null;
+		return object;
 	}
 
 	public Object getProperty(Object object, String propertyName, Object... qualifiers) {
